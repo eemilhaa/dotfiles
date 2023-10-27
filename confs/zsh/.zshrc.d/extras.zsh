b5575d3f3240f72838276f3bcab15204bd0b82e0
eemilhaa <eemil.haapanen@gmail.com>
2023-10-27 13:44:57 +0300

    add alias for host exec

--- confs/zsh/.zshrc.d/extras.zsh
+++ confs/zsh/.zshrc.d/extras.zsh
@@ -4,6 +4,7 @@ define_aliases () {
   alias db="distrobox"
   alias dbinit="distrobox create -n dev --image ghcr.io/eemilhaa/dotfiles:main"
   alias dbe="distrobox enter dev"
+  alias dbh="distrobox-host-exec"
   dev () {
     podman run -it --network host --rm -v $1:/root/work/:z ghcr.io/eemilhaa/dotfiles:main
   }
