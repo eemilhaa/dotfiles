12ca57314b0d7f4598f2291182d8b92e16404532
eemilhaa <eemil.haapanen@gmail.com>
2023-11-23 11:21:46 +0200

    fedora: use newest font with script too

--- scripts/font.sh
+++ scripts/font.sh
@@ -1,3 +1,8 @@
+font_dir="$HOME/.local/share/fonts/"
+mkdir -p $font_dir
+
+echo "installing font to $font_dir"
+
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/IosevkaTerm.zip
-unzip IosevkaTerm.zip -d ~/.local/share/fonts/
-fc-cache -f
+unzip IosevkaTerm.zip -d $font_dir
+fc-cache -fv
