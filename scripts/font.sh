27bb0df489492b82fd74bccfbf03d5361de6b2a4
eemilhaa <eemil.haapanen@gmail.com>
2024-01-15 14:27:52 +0200

    update font script

--- scripts/font.sh
+++ scripts/font.sh
@@ -1,8 +1,9 @@
+font_name='IosevkaTerm'
 font_dir="$HOME/.local/share/fonts/"
 mkdir -p $font_dir
 
 echo "installing font to $font_dir"
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/IosevkaTerm.zip
-unzip IosevkaTerm.zip -d $font_dir
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/$font_name.zip"
+unzip "$font_name.zip" -d $font_dir
 fc-cache -fv
