dade370d75f3721490b95e02eec32cca7f1d5170
eemilhaa <eemil.haapanen@gmail.com>
2024-06-25 11:15:02 +0300

    sway: screenshot window, waybar: sway window

---
+++ confs/.config/sway/screenshot_window.sh
@@ -0,0 +1,1 @@
+grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | "\(.rect.x+.window_rect.x),\(.rect.y+.window_rect.y) \(.window_rect.width)x\(.window_rect.height)"' | slurp)" - | wl-copy
