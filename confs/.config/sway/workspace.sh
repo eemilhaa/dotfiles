287800309569d162fb00bedfee7451c66e63a213
eemilhaa <eemil.haapanen@gmail.com>
2024-08-15 11:08:28 +0300

    format

--- confs/.config/sway/workspace.sh
+++ confs/.config/sway/workspace.sh
@@ -4,11 +4,11 @@ workspaces=$(swaymsg -t get_workspaces | jq -r '.[].name')
 
 if [ "$1" == "go_to_workspace" ]
 then
-  workspace=$(echo "$workspaces"| fuzzel --dmenu --no-fuzzy --prompt 'Go to: ')
+  workspace=$(echo "$workspaces" | fuzzel --dmenu --no-fuzzy --prompt 'Go to: ')
   swaymsg "workspace $workspace"
 elif [ "$1" == "move_to_workspace" ]
 then
-  workspace=$(echo "$workspaces"| fuzzel --dmenu --no-fuzzy --prompt 'Move to: ')
+  workspace=$(echo "$workspaces" | fuzzel --dmenu --no-fuzzy --prompt 'Move to: ')
   swaymsg "move container to workspace $workspace"
   swaymsg "workspace $workspace"
 fi
