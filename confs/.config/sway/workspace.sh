cc00f4222af3ae49134dc7316bafe1a4f96af75f
eemilhaa <eemil.haapanen@gmail.com>
2024-09-03 08:24:00 +0300

    cleaner workspace switcher

--- confs/.config/sway/workspace.sh
+++ confs/.config/sway/workspace.sh
@@ -2,13 +2,14 @@
 
 workspaces=$(swaymsg -t get_workspaces | jq -r '.[].name')
 
-if [ "$1" == "go_to_workspace" ]
-then
-  workspace=$(echo "$workspaces" | fuzzel --dmenu --no-fuzzy --prompt 'Go to: ')
-  swaymsg "workspace $workspace"
-elif [ "$1" == "move_to_workspace" ]
-then
-  workspace=$(echo "$workspaces" | fuzzel --dmenu --no-fuzzy --prompt 'Move to: ')
-  swaymsg "move container to workspace $workspace"
-  swaymsg "workspace $workspace"
-fi
+case "$1" in
+  go_to_workspace)
+    workspace=$(echo "$workspaces" | fuzzel --dmenu --no-fuzzy --prompt 'Go to: ')
+    swaymsg "workspace $workspace"
+  ;;
+  move_to_workspace)
+    workspace=$(echo "$workspaces" | fuzzel --dmenu --no-fuzzy --prompt 'Move to: ')
+    swaymsg "move container to workspace $workspace"
+    swaymsg "workspace $workspace"
+  ;;
+esac
