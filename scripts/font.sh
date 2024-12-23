3ccdc3a0206ad7b4888f0813d7086bc8db6f08aa
eemilhaa <eemil.haapanen@gmail.com>
2024-12-23 13:18:42 +0200

    bump font version

--- scripts/font.sh
+++ scripts/font.sh
@@ -4,6 +4,6 @@ mkdir -p $font_dir
 
 echo "installing font to $font_dir"
 
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/$font_name.zip"
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/$font_name.zip"
 unzip "$font_name.zip" -d $font_dir && rm "$font_name.zip"
 fc-cache -fv
