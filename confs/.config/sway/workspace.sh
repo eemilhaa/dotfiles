d534f8b9d9c7038766f1d3149d3d32af28f2d1c9
eemilhaa <eemil.haapanen@gmail.com>
2025-01-05 17:16:17 +0200

    fuzzel: config to 1.11

--- confs/.config/sway/workspace.sh
+++ confs/.config/sway/workspace.sh
@@ -4,11 +4,11 @@ workspaces=$(swaymsg -t get_workspaces | jq -r '.[].name')
 
 case "$1" in
   go_to_workspace)
-    workspace=$(echo "$workspaces" | fuzzel --dmenu --no-fuzzy --prompt 'Go to: ')
+    workspace=$(echo "$workspaces" | fuzzel --dmenu --match-mode exact --placeholder 'Go to workspace')
     swaymsg "workspace $workspace"
   ;;
   move_to_workspace)
-    workspace=$(echo "$workspaces" | fuzzel --dmenu --no-fuzzy --prompt 'Move to: ')
+    workspace=$(echo "$workspaces" | fuzzel --dmenu --match-mode exact --placeholder 'Move to workspace')
     swaymsg "move container to workspace $workspace"
     swaymsg "workspace $workspace"
   ;;
