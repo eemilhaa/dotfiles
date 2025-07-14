0e1aa011ddac933fa1ef566828ec8ae25b7a1c4d
eemilhaa <eemil.haapanen@gmail.com>
2025-07-14 10:59:19 +0300

    cleanup scripts & pkgs

--- scripts/ostree-base.sh
+++
@@ -1,9 +0,0 @@
-rpm-ostree install --allow-inactive --idempotent \
-  foot \
-  distrobox \
-  podman-compose \
-  zsh
-
-#rpm-ostree override remove \
-#  gnome-software \
-#  gnome-software-rpm-ostree
